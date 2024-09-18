390ab851856b2cd3392481246d767c79048dd6d6
Nelson Alves Pereira Filho <nelson.apf@gmail.com>
2024-09-18 17:49:44 -0300

    Avancando no cap 4 (SERVICES)

--- src/main/java/com/nelson/estudos/java/user_api/repository/UserRepository.java
+++ src/main/java/com/nelson/estudos/java/user_api/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.nelson.estudos.java.user_api.repository;
 
+import com.nelson.estudos.java.user_api.dto.UserDTO;
 import com.nelson.estudos.java.user_api.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +15,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByCpf (String cpf);
 
     List<User> queryByNomeLike (String name);
+
 }
--- src/main/java/com/nelson/estudos/java/user_api/service/UserService.java
+++ src/main/java/com/nelson/estudos/java/user_api/service/UserService.java
@@ -4,8 +4,11 @@ import com.nelson.estudos.java.user_api.dto.UserDTO;
 import com.nelson.estudos.java.user_api.model.User;
 import com.nelson.estudos.java.user_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,11 +18,78 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    //################## RETORNA LISTA DE DTOS #######################
     public List<UserDTO> getAll() {
         List<User> usuarios = userRepository.findAll();
         return usuarios
                 .stream()
                 .map(UserDTO::convert)
                 .collect(Collectors.toList());
+    }
+
+    //################## BUSCA USUER POR ID ###########################
+    public UserDTO findById (long userId) {
+        User usuario = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuario nao encontrado"));
+        return UserDTO.convert(usuario);
+    }
+
+    //################## SALVA USUARIO ################################
+    public UserDTO save(UserDTO userDTO) {
+        userDTO.setDataCadastro(LocalDateTime.now());
+        User user = userRepository.save(User.convert(userDTO));
+        return UserDTO.convert(user);
+    }
+
+    //################## DELETA USUARIO ###############################
+    public UserDTO delete(long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuario nao encontrado ao deletar"));
+        userRepository.delete(user);
+        return UserDTO.convert(user);
+    }
+
+    //################## BUSCA POR CPF ################################
+    public UserDTO findByCpf(String cpf) {
+        User user = userRepository.findByCpf(cpf);
+        if (user != null) {
+            return UserDTO.convert(user);
+        }
+        return null;
+    }
+
+    //################## BUSCA POR NOME ################################
+    public List<UserDTO> queryByName(String name) {
+        List<User> usuarios = userRepository.queryByNomeLike(name);
+        return usuarios.stream()
+                .map(UserDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    //################## EDITA USUARIO #################################
+    public UserDTO editUser (Long userId, UserDTO userDTO) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuario nao encontrado ao editar"));
+        if (userDTO.getEmail() != null &&
+                !user.getEmail().equals(userDTO.getEmail())) {
+            user.setEmail(userDTO.getEmail());
+        }
+        if (userDTO.getTelefone() != null &&
+                !user.getTelefone().equals(userDTO.getTelefone())) {
+            user.setTelefone(userDTO.getTelefone());
+        }
+        if (userDTO.getEndereco() != null &&
+                !user.getEndereco().equals(userDTO.getEndereco())) {
+            user.setEndereco(userDTO.getEndereco());
+        }
+
+        user = userRepository.save(user);
+        return UserDTO.convert(user);
+    }
+
+    //################## ENTENDER FUNCIONAMENTO #########################
+    public Page<UserDTO> getAllPage (Pageable page) {
+        Page<User> users = userRepository.findAll(page);
+        return users.map(UserDTO::convert);
     }
 }
